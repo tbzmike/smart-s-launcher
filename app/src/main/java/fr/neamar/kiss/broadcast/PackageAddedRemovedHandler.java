1fb69c6d5ab225aec2ce87bf7102474f42382c0a
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-08-19 11:19:42 +0200

    Forget app catalog entries only on real uninstall

--- app/src/main/java/fr/neamar/kiss/broadcast/PackageAddedRemovedHandler.java
+++ app/src/main/java/fr/neamar/kiss/broadcast/PackageAddedRemovedHandler.java
@@ -12,34 +12,20 @@ import androidx.preference.PreferenceManager;
 import java.util.Set;
 
 import fr.neamar.kiss.KissApplication;
+import fr.neamar.kiss.db.SmartStateStore;
 import fr.neamar.kiss.pojo.AppPojo;
 import fr.neamar.kiss.utils.PackageManagerUtils;
 import fr.neamar.kiss.utils.UserHandle;
 
-/**
- * This class gets called when an application is created or removed on the
- * system
- * <p/>
- * We then recreate our data set.
- *
- * @author dorvaryn
- */
 public class PackageAddedRemovedHandler extends BroadcastReceiver {
 
     public static void handleEvent(@NonNull Context ctx, @Nullable String action, @NonNull String[] packageNames, @NonNull UserHandle user, boolean replacing) {
-        if (packageNames.length == 1 && packageNames[0].equalsIgnoreCase(ctx.getPackageName())) {
-            // When running KISS locally, sending a new version of the APK immediately triggers a "package removed" for fr.neamar.kiss,
-            // There is no need to handle this event.
-            // Discarding it makes startup time much faster locally as apps don't have to be loaded twice.
-            return;
-        }
+        if (packageNames.length == 1 && packageNames[0].equalsIgnoreCase(ctx.getPackageName())) return;
 
-        if (Intent.ACTION_PACKAGE_ADDED.equals(action)) {
-            if (!replacing) {
-                for (String packageName : packageNames) {
-                    if (PreferenceManager.getDefaultSharedPreferences(ctx).getBoolean("enable-app-history", true)) {
-                        KissApplication.getApplication(ctx).getDataHandler().addPackageToHistory(ctx, user, packageName);
-                    }
+        if (Intent.ACTION_PACKAGE_ADDED.equals(action) && !replacing) {
+            for (String packageName : packageNames) {
+                if (PreferenceManager.getDefaultSharedPreferences(ctx).getBoolean("enable-app-history", true)) {
+                    KissApplication.getApplication(ctx).getDataHandler().addPackageToHistory(ctx, user, packageName);
                 }
             }
         }
@@ -49,42 +35,30 @@ public class PackageAddedRemovedHandler extends BroadcastReceiver {
         if (Intent.ACTION_PACKAGE_REMOVED.equals(action)) {
             if (!replacing) {
                 KissApplication.getApplication(ctx).resetIconsHandler();
-                // Reload application list
                 KissApplication.getApplication(ctx).getDataHandler().reloadApps();
-                // Remove all installed shortcuts
                 for (String packageName : packageNames) {
                     KissApplication.getApplication(ctx).getDataHandler().removeShortcuts(packageName);
                     KissApplication.getApplication(ctx).getDataHandler().removeFromExcluded(packageName);
+                    // A genuine uninstall is the only time a remembered frozen app should vanish.
+                    SmartStateStore.forgetPackage(ctx, packageName);
                 }
             }
         } else {
             KissApplication.getApplication(ctx).resetIconsHandler();
-
-            boolean isAnyPackageVisible = isAnyPackageVisible(ctx, packageNames, user);
-            if (isAnyPackageVisible) {
-                // Reload application list
-                KissApplication.getApplication(ctx).getDataHandler().reloadApps();
-                // Reload shortcuts
-                KissApplication.getApplication(ctx).getDataHandler().reloadShortcuts();
-            }
+            // Package changed/available events include freeze/unfreeze transitions. Always reload:
+            // relying on getLaunchingComponent() would hide exactly the packages IceBox disabled.
+            KissApplication.getApplication(ctx).getDataHandler().reloadApps();
+            KissApplication.getApplication(ctx).getDataHandler().reloadShortcuts();
         }
     }
 
-    /**
-     * @param ctx
-     * @param packageNames
-     * @param userHandle
-     * @return true, if any of packages has activity for launching and is not excluded from KISS
-     */
     private static boolean isAnyPackageVisible(Context ctx, String[] packageNames, UserHandle userHandle) {
         Set<String> excludedApps = KissApplication.getApplication(ctx).getDataHandler().getExcluded();
         for (String packageName : packageNames) {
             ComponentName launchingComponent = PackageManagerUtils.getLaunchingComponent(ctx, packageName, userHandle);
             if (launchingComponent != null) {
                 boolean isExcluded = excludedApps.contains(AppPojo.getComponentName(launchingComponent.getPackageName(), launchingComponent.getClassName(), userHandle));
-                if (!isExcluded) {
-                    return true;
-                }
+                if (!isExcluded) return true;
             }
         }
         return false;
@@ -97,16 +71,8 @@ public class PackageAddedRemovedHandler extends BroadcastReceiver {
             String packageName = intent.getData().getSchemeSpecificPart();
             packageNames = new String[]{packageName};
         }
-        if (packageNames == null) {
-            return;
-        }
-
-        handleEvent(ctx,
-                intent.getAction(),
-                packageNames, UserHandle.OWNER,
-                intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)
-        );
-
+        if (packageNames == null) return;
+        handleEvent(ctx, intent.getAction(), packageNames, UserHandle.OWNER,
+                intent.getBooleanExtra(Intent.EXTRA_REPLACING, false));
     }
-
 }
