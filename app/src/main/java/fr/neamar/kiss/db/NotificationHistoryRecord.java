77bcd1dab8a0d6ec2f48dc099f81dc779b61585e
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-08-19 11:17:25 +0200

    Add notification history record

---
+++ app/src/main/java/fr/neamar/kiss/db/NotificationHistoryRecord.java
@@ -0,0 +1,11 @@
+package fr.neamar.kiss.db;
+
+public class NotificationHistoryRecord {
+    public long dbId;
+    public String notificationId;
+    public String packageName;
+    public String appName;
+    public String title;
+    public String text;
+    public long postTime;
+}
