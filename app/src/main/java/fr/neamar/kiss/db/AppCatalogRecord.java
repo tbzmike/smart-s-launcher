511c83ed379eba9ee57479912e48c8f9aa414501
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-08-19 11:17:19 +0200

    Add persistent app catalog record

---
+++ app/src/main/java/fr/neamar/kiss/db/AppCatalogRecord.java
@@ -0,0 +1,8 @@
+package fr.neamar.kiss.db;
+
+public class AppCatalogRecord {
+    public String packageName;
+    public String activityName;
+    public String label;
+    public long userSerial;
+}
