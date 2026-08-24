2cf35623a25357de64b412035ead2e3aed8e16f3
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-08-24 08:10:37 +0200

    Add coarse location permission for live Maps tile

--- app/src/main/java/fr/neamar/kiss/utils/Permission.java
+++ app/src/main/java/fr/neamar/kiss/utils/Permission.java
@@ -17,17 +17,16 @@ public class Permission {
     public static final int PERMISSION_READ_CONTACTS = 0;
     public static final int PERMISSION_CALL_PHONE = 1;
     public static final int PERMISSION_READ_PHONE_STATE = 2;
+    public static final int PERMISSION_ACCESS_COARSE_LOCATION = 3;
 
     private static final String[] permissions = {
             Manifest.permission.READ_CONTACTS,
             Manifest.permission.CALL_PHONE,
-            Manifest.permission.READ_PHONE_STATE
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.ACCESS_COARSE_LOCATION
     };
 
-    // Static weak reference to the linked activity, this is sadly required
-    // to ensure classes requesting permission can access activity.requestPermission()
     private static WeakReference<Activity> currentActivity = new WeakReference<>(null);
-
     private static final ArrayList<PermissionResultListener> permissionListeners = new ArrayList<>();
 
     public static boolean checkPermission(Context context, int permission) {
@@ -35,41 +34,26 @@ public class Permission {
     }
 
     public static void askPermission(int permission, PermissionResultListener listener) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return;
-        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return;
         if (listener != null) {
             listener.permission = permission;
             permissionListeners.add(listener);
         }
-
         Activity activity = Permission.currentActivity.get();
-        if (activity != null) {
-            activity.requestPermissions(new String[]{permissions[permission]}, permission);
-        }
+        if (activity != null) activity.requestPermissions(new String[]{permissions[permission]}, permission);
     }
 
-    public Permission(Activity activity) {
-        // Store the latest reference to a MainActivity
-        currentActivity = new WeakReference<>(activity);
-    }
+    public Permission(Activity activity) { currentActivity = new WeakReference<>(activity); }
 
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (grantResults.length == 0) {
-            return;
-        }
-
-        // Iterator allows to remove while iterating
+        if (grantResults.length == 0) return;
         ListIterator<PermissionResultListener> it = permissionListeners.listIterator();
         PermissionResultListener permissionListener;
         while (it.hasNext()) {
             permissionListener = it.next();
             if (permissionListener.permission == requestCode) {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    permissionListener.onGranted();
-                } else {
-                    permissionListener.onDenied();
-                }
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) permissionListener.onGranted();
+                else permissionListener.onDenied();
                 it.remove();
             }
         }
@@ -77,7 +61,6 @@ public class Permission {
 
     public static class PermissionResultListener {
         public int permission = 0;
-
         public void onGranted() {}
         public void onDenied() {}
     }
