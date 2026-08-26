0af63507b5e79707e99f6308166eee143323bb33
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-08-26 10:51:43 +0200

    Test recent launch cache keeps older history entries resolvable

---
+++ app/src/test/java/fr/neamar/kiss/utils/RecentLaunchTrackerTest.java
@@ -0,0 +1,24 @@
+package fr.neamar.kiss.utils;
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+import org.junit.jupiter.api.Test;
+
+import fr.neamar.kiss.pojo.Pojo;
+
+class RecentLaunchTrackerTest {
+
+    @Test
+    void olderSelectionRemainsResolvableAfterNewerSelection() {
+        Pojo first = new Pojo("shortcut://first");
+        first.setName("First shortcut");
+        Pojo second = new Pojo("app://second");
+        second.setName("Second app");
+
+        RecentLaunchTracker.remember(first);
+        RecentLaunchTracker.remember(second);
+
+        assertSame(first, RecentLaunchTracker.resolve(first.getHistoryId()));
+        assertSame(second, RecentLaunchTracker.getMostRecent());
+    }
+}
