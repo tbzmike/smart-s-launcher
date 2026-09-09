496d4a63c69811351e909cc34bb776c746394e2c
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-09-09 20:29:45 +0200

    Test the exact empty-pattern crash path

--- app/src/test/java/fr/neamar/kiss/utils/fuzzy/MatchInfoTest.java
+++ app/src/test/java/fr/neamar/kiss/utils/fuzzy/MatchInfoTest.java
@@ -1,6 +1,7 @@
 package fr.neamar.kiss.utils.fuzzy;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.nullValue;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,14 @@ class MatchInfoTest {
         MatchInfo info = new MatchInfo(0);
         info.match = true;
 
+        assertThat(info.getMatchedIndices(), nullValue());
+    }
+
+    @Test
+    void emptyFuzzyV1PatternCannotExposeCrashableIndexList() {
+        MatchInfo info = new FuzzyScoreV1(new int[0], true).match(new int[] {'A'});
+
+        assertThat(info.match, equalTo(true));
         assertThat(info.getMatchedIndices(), nullValue());
     }
 }
