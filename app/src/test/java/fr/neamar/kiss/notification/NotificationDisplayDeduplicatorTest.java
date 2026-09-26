739e1ea59a5d047f875d7eb3804e683ec08b2482
tbzmike <40115947+tbzmike@users.noreply.github.com>
2026-09-26 05:58:15 -0700

    Test notification display deduplication

---
+++ app/src/test/java/fr/neamar/kiss/notification/NotificationDisplayDeduplicatorTest.java
@@ -0,0 +1,36 @@
+package fr.neamar.kiss.notification;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class NotificationDisplayDeduplicatorTest {
+    @Test void collapsesSameVisibleEventFromTwoAndroidSlots() {
+        assertThat(NotificationDisplayDeduplicator.isNearDuplicate(
+                "com.whatsapp", "chat-group", "Andronicah", "Bread and chips", 1_000_000L,
+                "com.whatsapp", "chat-group", " Andronicah ", "Bread   and chips", 1_020_000L),
+                is(true));
+    }
+
+    @Test void keepsSameTextWhenEventsAreFarApart() {
+        assertThat(NotificationDisplayDeduplicator.isNearDuplicate(
+                "com.whatsapp", "chat-group", "Andronicah", "Bread and chips", 1_000_000L,
+                "com.whatsapp", "chat-group", "Andronicah", "Bread and chips", 1_200_000L),
+                is(false));
+    }
+
+    @Test void keepsDifferentNotificationGroupsSeparate() {
+        assertThat(NotificationDisplayDeduplicator.isNearDuplicate(
+                "com.whatsapp", "chat-a", "Message", "Hello", 1_000_000L,
+                "com.whatsapp", "chat-b", "Message", "Hello", 1_010_000L),
+                is(false));
+    }
+
+    @Test void emptyPresentationIsNeverCollapsed() {
+        assertThat(NotificationDisplayDeduplicator.isNearDuplicate(
+                "android", "system", "", "", 1_000_000L,
+                "android", "system", "", "", 1_001_000L),
+                is(false));
+    }
+}
